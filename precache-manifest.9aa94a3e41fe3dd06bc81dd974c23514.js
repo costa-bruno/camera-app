f8a91e526027a290700cd6ed6002bb36a2735453
bruno.costa <bruno.costa@stormsec.com.br>
2020-01-25 18:34:12 -0300

    Updates

---
+++ precache-manifest.9aa94a3e41fe3dd06bc81dd974c23514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b207c9f804a07df9b9fd21732e9f6bd",
+    "url": "/camera-app/index.html"
+  },
+  {
+    "revision": "f2a8e59f1ce898b25609",
+    "url": "/camera-app/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "e4a0625800be2664cb89",
+    "url": "/camera-app/static/css/main.253951c8.chunk.css"
+  },
+  {
+    "revision": "f2a8e59f1ce898b25609",
+    "url": "/camera-app/static/js/2.920308ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/camera-app/static/js/2.920308ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4a0625800be2664cb89",
+    "url": "/camera-app/static/js/main.2f00fff5.chunk.js"
+  },
+  {
+    "revision": "24c336623a4a086b360c",
+    "url": "/camera-app/static/js/runtime-main.fc6b5f66.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/camera-app/precache-manifest.9aa94a3e41fe3dd06bc81dd974c23514.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/camera-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
